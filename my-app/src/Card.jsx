68065ebe015715c0d33fb2d9b2a475c0e69bd5f2
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-05-12 00:02:48 +0400

    hello

--- my-app/src/Card.jsx
+++ my-app/src/Card.jsx
@@ -1,6 +1,6 @@
 import Card from '@mui/material/Card';
 import * as React from 'react';
-import CardActions from '@mui/material/CardActions';
+// import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
